1d0504085606b1bd51300bb06b22dc458fe4cfa5
Simonianarthur <arthur.simonian@outlook;fr>
2019-10-03 23:11:26 +0200

    test user and product
    test userController , productController ; add label in userType; modify User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -23,6 +24,7 @@ class User {
      * @ORM\Column(type="string", length=255)
      * @Groups({"user.lite"})
      * @Groups({"user"})
+     * @Assert\NotNull
      */
     private $lastName;
 
